45089babce21ccc92c8ea6d3a0573024842dad18
jonyurisoon <jonyurisoon@gmail.com>
2024-05-05 01:40:33 +0800

    on going dashboard ui

--- app/src/main/java/com/example/philantrolink/CharityEventDetailsActivity.kt
+++ app/src/main/java/com/example/philantrolink/CharityEventDetailsActivity.kt
@@ -7,36 +7,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.philantrolink.databinding.ActivityCreateEventBinding
+import com.example.philantrolink.databinding.ActivityCharityEventDetailsBinding
 
-class CreateEventActivity : AppCompatActivity() {
+class CharityEventDetailsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityCreateEventBinding
+    private lateinit var binding: ActivityCharityEventDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCreateEventBinding.inflate(layoutInflater)
+        binding = ActivityCharityEventDetailsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         // Set up the Toolbar
-        setSupportActionBar(binding.CreateEventToolbar)
+        setSupportActionBar(binding.CharityEventDetailsToolbar)
         // Get the action bar
         val actionBar = supportActionBar
         // Set the title
-        actionBar?.title = "Create Event"
+        actionBar?.title = "Charity Event Details"
         // Set title text color to white
-        binding.CreateEventToolbar.setTitleTextColor(Color.WHITE)
+        binding.CharityEventDetailsToolbar.setTitleTextColor(Color.WHITE)
         // Enable Up button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         // Set click listener for the Up button
-        binding.CreateEventToolbar.setNavigationOnClickListener {
+        binding.CharityEventDetailsToolbar.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        binding.CharityEventButton.setOnClickListener {
-            val intent = Intent(this, CharityEventDetailsActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
